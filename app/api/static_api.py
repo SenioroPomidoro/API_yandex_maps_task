d9de41efeef1bfa1d490041b089244a51ba340d2
ira <prijmenko.ira@mail.ru>
2025-04-12 20:22:15 +0700

    Задача №4, кнопка переключения темы

--- app/api/static_api.py
+++ app/api/static_api.py
@@ -3,9 +3,10 @@ import requests
 from app import const
 
 
-def get_map_image(scale: float, long_lat: list[float]) -> bytes:
+def get_map_image(scale: float, long_lat: list[float], theme: str) -> bytes:
     """
     Функция, получающая изображение части города из static_api и сохраняющая его в файл map.png
+    :param theme: цветовая тема light или dark
     :param long_lat: широта, долгота
     :param scale: масштаб
     """
@@ -13,6 +14,7 @@ def get_map_image(scale: float, long_lat: list[float]) -> bytes:
     params = {
         "apikey": const.STATIC_MAPS_API_KEY,
         "ll": f"{long_lat[0]},{long_lat[1]}",
+        "theme": theme,
         "z": scale
     }
 
