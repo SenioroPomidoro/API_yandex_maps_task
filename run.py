ebccc6139c153b6b4ea19f69c15e770e6e354591
ira <prijmenko.ira@mail.ru>
2025-04-13 13:05:21 +0700

    добавлено создание папки для хранения изображения карты

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+import os
+
+from app import const
 from app.main import QApplication, MapsApp, sys
+
+os.makedirs(const.MAP_IMAGE_PATH, exist_ok=True)
 
 app = QApplication(sys.argv)
 window = MapsApp()
